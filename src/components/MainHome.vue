f857d032db034eb94006d5506371709d1d306b53
LMerino420 <merinolev96@gmail.com>
2022-07-20 17:06:51 -0600

    Button Slide Movements

--- src/components/MainHome.vue
+++ src/components/MainHome.vue
@@ -1,3 +1,24 @@
 <template>
-	<div>HOLA MUNDO</div>
+	<MainLayout>
+		<template #header>
+			<MainHeader />
+		</template>
+		<template #resume>
+			<MainResume />
+		</template>
+		<template #movements>
+			<MainMovements />
+		</template>
+	</MainLayout>
 </template>
+
+<script>
+import MainLayout from "./layout/MainLayout.vue";
+import MainHeader from "./layout/MainHeader.vue";
+import MainResume from "./layout/MainResume.vue";
+import MainMovements from "./layout/MainMovements.vue";
+
+export default {
+	components: { MainLayout, MainHeader, MainResume, MainMovements },
+};
+</script>
---
+++ src/components/layout/MainHeader.vue
@@ -0,0 +1,37 @@
+<template>
+	<header>
+		<img src="@/assets/cashflow-icon.svg" alt="Logo de la app" @click="reload" />
+		<h1><span>Money</span>Flow</h1>
+		<span></span>
+	</header>
+</template>
+
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	width: 100vw;
+}
+img,
+span {
+	width: 48px;
+}
+h1 {
+	font-size: 1.4rem;
+	color: var(--brand-blue);
+}
+h1 span {
+	color: var(--brand-green);
+}
+</style>
+
+<script>
+export default {
+	methods: {
+		reload() {
+			location.reload();
+		},
+	},
+};
+</script>
