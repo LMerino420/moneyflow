8fef470a3fd1fefb1fa32da01a3dd2a3dfea55ef
LMerino420 <merinolev96@gmail.com>
2022-07-20 16:11:53 -0600

    SplashScreen

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="splashscreen">
+		<img src="@/assets/cashflow-icon.svg" alt="Logo de la app" />
+		<h1><span>Money</span>Flow</h1>
+	</div>
+</template>
+
+<style scoped>
+.splashscreen {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	height: 100vh;
+}
+h1 {
+	color: var(--brand-blue);
+}
+h1 span {
+	color: var(--brand-green);
+}
+</style>
